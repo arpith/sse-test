1a0ceb527f41ecbad5a98538642b6a5d5bcb45c8
Arpith <arpith@feedreader.co>
2015-05-28 22:24:55 +0530

    beginning to write tests

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 var util = require('util');
 var EventSource = require('eventsource');
-var dgram  = require('dgram');
-var apikey = process.env.HOSTEDGRAPHITE_APIKEY;
+var request = require('request');
 
-
-for (var i = 0; i < process.env.CLIENT_COUNT; i++) {
-  var k = i;
-  setTimeout(function () {
-    createClient(k);
-  }, i*100);
+for {
+  var clientCount = randomClientCount();
+  var messageCount = randomMessageCount(clientCount);
+  var channelId = randomChannelId();
+  var messagesReceived = {}
+  for (var i = 0; i < clientCount; i++) {
+    setTimeout(function (clientId, channelId, messageCount) {
+      createClient(clientId, channelId, messageCount);
+    }, i*100, i, channelId, messageCount);
+  }
+  for (var i = 0; i < messageCount; i++) {
+    messagesReceived[i] = 0;
+    request.post(process.env.SATELLITE_URL+channelId).form({'token':process.env.TOKEN, 'message':i});
+  }
 }
 
-function createClient(id) {
-  var es = new EventSource(process.env.SATELLITE_URL);
+function createClient(id, channelId) {
+  var es = new EventSource(process.env.SATELLITE_URL+channelId);
   es.onmessage = function(e) {
     console.log("Received: "+e.data+" ("+id+")");
   };
@@ -21,3 +28,18 @@ function createClient(id) {
   };
 }
 
+function randomChannelId() {
+  return Math.random().toString();
+}
+
+function randomMessageCount(clientCount) {
+  return getRandomInt(1, clientCount);
+}
+
+function randomClientCount() {
+  return getRandomInt(1, process.env.CLIENT_COUNT);
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
