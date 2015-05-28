4e8645d583734ff7106379c9d751c54f27da550e
Arpith <arpith@feedreader.co>
2015-05-28 21:42:58 +0530

    use graphite later

--- main.js
+++ main.js
@@ -3,10 +3,9 @@ var EventSource = require('eventsource');
 var dgram  = require('dgram');
 var apikey = process.env.HOSTEDGRAPHITE_APIKEY;
 
-var clients = []
-for(var i = 0; i < process.env.CLIENT_COUNT; i++){
+
+for (var i = 0; i < process.env.CLIENT_COUNT; i++) {
   var k = i;
-  clients[i] = 0;
   setTimeout(function () {
     createClient(k);
   }, i*100);
@@ -15,29 +14,10 @@ for(var i = 0; i < process.env.CLIENT_COUNT; i++){
 function createClient(id) {
   var es = new EventSource(process.env.SATELLITE_URL);
   es.onmessage = function(e) {
-    clients[id]++;
-    console.log(util.inspect(clients));
     console.log("Received: "+e.data+" ("+id+")");
-//    logSuccess(e.data);
   };
   es.onerror = function(e) {
     console.log("Error: "+id+" "+util.inspect(e))
-//    logError(id);
   };
 }
 
-function logSuccess(data) {
-  var message = new Buffer(apikey + "."+data+".success 1\n");
-  var client = dgram.createSocket("udp4");
-  client.send(message, 0, message.length, 2003, "carbon.hostedgraphite.com", function(err, bytes) {
-    client.close();
-  });
-}
-
-function logError(id) {
-  var message = new Buffer(apikey + ".errors."+id+" 1\n");
-  var client = dgram.createSocket("udp4");
-  client.send(message, 0, message.length, 2003, "carbon.hostedgraphite.com", function(err, bytes) {
-    client.close();
-  });
-}
