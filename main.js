f793238afe379a65d35a0f86ac68d70ac3e32cdf
Arpith Siromoney <arpith@feedreader.co>
2015-05-28 15:31:02 +0530

    Log to graphite

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var EventSource = require('eventsource');
+var dgram  = require('dgram');
+var apikey = process.env.HOSTEDGRAPHITE_APIKEY;
 
 for(var i = 0; i < process.env.CLIENT_COUNT; i++){
   createClient(i);
@@ -7,9 +9,25 @@ for(var i = 0; i < process.env.CLIENT_COUNT; i++){
 function createClient(id) {
   var es = new EventSource(process.env.SATELLITE_URL);
   es.onmessage = function(e) {
-    console.log(id+': '+e.data);
+    logSuccess(e.data);
   };
   es.onerror = function() {
-    console.log(id+': ERROR');
+    logError(id);
   };
+}
+
+function logSuccess(data) {
+  var message = new Buffer(apikey + "."+data+".success 1\n");
+  var client = dgram.createSocket("udp4");
+  client.send(message, 0, message.length, 2003, "carbon.hostedgraphite.com", function(err, bytes) {
+    client.close();
+  });
+}
+
+function logError(id) {
+  var message = new Buffer(apikey + ".errors."+id+" 1\n");
+  var client = dgram.createSocket("udp4");
+  client.send(message, 0, message.length, 2003, "carbon.hostedgraphite.com", function(err, bytes) {
+    client.close();
+  });
 }
