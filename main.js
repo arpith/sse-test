92b513e1e54719f9464fd7dad077d85aa497f8ca
Arpith <arpith@feedreader.co>
2015-05-29 22:08:37 +0530

    new test

---
+++ main.js
@@ -0,0 +1,65 @@
+var deferred = require('simply-deferred');
+var satelliteUrl = "https://sattest1.herokuapp.com";
+var url = satelliteUrl+'/broadcast';
+
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+var EventSource = require('EventSource');
+var request = require('request');
+var maxClientCount = 100;
+
+class Client {
+  constructor(url) {
+    this.url = url;
+    this.es = new EventSource(url);
+    this.emitter = new EventEmitter();
+    this.es.onmessage = (e => this.emitter.emit('message', e.data));
+    this.loader = new deferred.Deferred();
+    this.es.onopen = (e => this.loader.resolve());
+  }
+  waitOn(expectedMessage, timeout) {
+    var promise = new deferred.Deferred();
+    this.emitter.on('message', function (msg) {
+      if (msg === expectedMessage)
+        promise.resolve();
+    });
+    setTimeout(function(){promise.reject();},timeout);
+    return promise.promise();
+  }
+}
+var randomChannelId = function() {
+  var crypto = require('crypto')
+    , shasum = crypto.createHash('sha1');
+   shasum.update(Math.random().toString());
+  return shasum.digest('hex');
+}
+
+var randomMessageCount = function(clientCount) {
+  return getRandomInt(1, clientCount);
+}
+
+var randomClientCount = function() {
+  return getRandomInt(1, maxClientCount);
+}
+
+var getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var startTest = function () {
+  var channelUrl = url+'/'+randomChannelId();
+  var clientCount = randomClientCount();
+  var clients = []
+  for (let i=0; i<clientCount; i++) {
+    clients.push(new Client(channelUrl));
+  }
+  var msg = randomChannelId();
+  var clientLoaders = clients.map(c => c.loader);
+  deferred.when(clientLoaders).done(function() {
+    var clientPromises = clients.map(c => c.waitOn(msg,5000));
+    deferred.when(clientPromises).done(function() {console.log("Works for message "+msg+" count "+clientCount);});
+    deferred.when(clientPromises).fail(function() {console.log("Fail for message "+msg+" count "+clientCount);});
+    request.post(channelUrl).form({'token':'abcd1234','message':msg});
+  });
+}
+setInterval(function(){startTest()}, 5000);
