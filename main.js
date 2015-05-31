3366a4cb3d9787aadaaa7e0bbb2e83a1a646650d
Arpith Siromoney <arpith@feedreader.co>
2015-05-31 10:54:55 +0530

    Read environment variables

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 var EventSource = require('EventSource');
 var request = require('request');
-var maxClientCount = 100;
+var maxClientCount = process.env.CLIENT_COUNT;
+var token = process.env.TOKEN;
+var url = process.env.SATELLITE_URL + '/broadcast/';
+
 
 class Client {
   constructor(url) {
@@ -27,6 +30,7 @@ class Client {
     return promise.promise();
   }
 }
+
 var randomChannelId = function() {
   var crypto = require('crypto')
     , shasum = crypto.createHash('sha1');
@@ -59,7 +63,7 @@ var startTest = function () {
     var clientPromises = clients.map(c => c.waitOn(msg,5000));
     deferred.when(clientPromises).done(function() {console.log("Works for message "+msg+" count "+clientCount);});
     deferred.when(clientPromises).fail(function() {console.log("Fail for message "+msg+" count "+clientCount);});
-    request.post(channelUrl).form({'token':'abcd1234','message':msg});
+    request.post(channelUrl).form({'token':token,'message':msg});
   });
 }
 setInterval(function(){startTest()}, 5000);
