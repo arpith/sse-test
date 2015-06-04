25c786ac3408a3885cacc967244b2b64adfadfb5
Arpith Siromoney <arpith@feedreader.co>
2015-06-04 18:30:19 +0530

    just getting a feel of promises

--- with-promises.js
+++ with-promises.js
@@ -12,38 +12,30 @@ var satelliteUrl = process.env.SATELLITE_URL + '/broadcast';
 class Client {
   constructor(url) {
     this.url = url;
-    this.es = new EventSource(url);
-    this.emitter = new EventEmitter();
-    this.loader = new deferred.Deferred();
-    var loader = this.loader;
-    this.es.onopen = function(){ loader.resolve()};
-    this.es.onerror = (e => console.log(util.inspect(e)));
-    this.es.onmessage = (e => loader.resolve() && this.emitter.emit('message', e.data));
   }
-  waitOn(expectedMessage, timeout) {
-    var promise = new deferred.Deferred();
-    this.emitter.on('message', function (msg) {
-      if (msg === 'PONG')
-        promise.resolve();
-    });
-    setTimeout(function(){promise.reject();},timeout);
-    return promise.promise();
-  }
+	addES() {
+		return new Promise(function(resolve, reject) {
+			this.es = new EventSource(this.url);
+			this.es.onopen = resolve;
+			this.es.onerror = reject;
+		})
+	}
+	receiveMessage() {
+		return new Promise(function(resolve, reject) {
+			this.es.onerror = reject;
+			this.es.onmessage = resolve;
+		})
+	}
   close() {
-//    this.es.close();
+    this.es.close();
   }
-
 }
 
-var randomChannelId = function() {
+var randomString = function() {
   var crypto = require('crypto')
     , shasum = crypto.createHash('sha1');
    shasum.update(Math.random().toString());
   return shasum.digest('hex');
-}
-
-var randomMessageCount = function(clientCount) {
-  return getRandomInt(1, clientCount);
 }
 
 var randomClientCount = function() {
@@ -55,19 +47,24 @@ var getRandomInt = function(min, max) {
 }
 
 var startTest = function () {
-  var channelUrl = satelliteUrl+'/'+randomChannelId();
+  var msg = randomString();
+  var channelUrl = satelliteUrl+'/'+randomString();
   var clientCount = randomClientCount();
-  var clients = []
+	var postMessage = function() {
+		return new Promise(function(resolve, reject) {
+			request
+				.post(channelUrl)
+				.form({'token':token,'message':msg})
+			  .on('error', reject)
+				.on('response', resolve);
+		});
+	}
+  var clients = [];
   for (let i=0; i<clientCount; i++) {
     clients.push(new Client(channelUrl));
   }
-  var msg = randomChannelId();
-  var clientLoaders = clients.map(c => c.loader);
-  deferred.when(clientLoaders).done(function() {
-    var clientPromises = clients.map(c => c.waitOn(msg,20000));
-    deferred.when(clientPromises).done(function() {clients.map(c => c.close()); console.log("Works for message "+msg+" count "+clientCount);});
-    deferred.when(clientPromises).fail(function() {clients.map(c => c.close()); console.log("Fail for message "+msg+" count "+clientCount);});
-    request.post(channelUrl).form({'token':token,'message':msg});
-  });
+	Promise.all(clients.map(c => c.addEs))
+		.then(function(arrayOfResults) {console.log(util.inspect(arrayOfResults));})
+		.catch(function(arrayOfErrors) {console.log(util.inspect(arrayOfErrors));});
 }
 setInterval(function(){startTest()}, 5000);
